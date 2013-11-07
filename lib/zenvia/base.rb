59e007db739db0502bb4febaec593be80b0a3f38
leonelmorais <leonel.morais@yahoo.com.br>
2013-11-07 00:36:50 -0200

    Change base class to module

--- lib/zenvia/base.rb
+++ lib/zenvia/base.rb
@@ -1,2 +1,23 @@
-module Base
+# encoding: UTF-8
+require 'net/http'
+require 'uri'
+
+module Zenvia
+  module Base
+    ZENVIA_URL = 'http://www.zenvia360.com.br/GatewayIntegration/msgSms.do'
+
+    private
+
+    def send_to_zenvia(cel_phone, msg)
+      Net::HTTP.post_form URI(ZENVIA_URL),
+        {
+          :account  => Zenvia.configuration.account,
+          :code     => Zenvia.configuration.code,
+          :dispatch => Zenvia.configuration.dispatch,
+          :from     => Zenvia.configuration.from,
+          :to       => cel_phone,
+          :msg      => msg
+        }
+    end
+  end
 end
