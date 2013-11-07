dd0c0b52104587e6100c146e3c09697652f49e89
leonelmorais <leonel.morais@yahoo.com.br>
2013-11-07 00:39:41 -0200

    Add class Sms to implement method that forward sms

---
+++ lib/zenvia/sms.rb
@@ -0,0 +1,18 @@
+require './zenvia/base'
+
+module Zenvia
+  class Sms
+    include Base
+
+    attr_accessor :msg, :cel_phone
+
+    def initialize(msg, cel_phone)
+      @msg       = msg
+      @cel_phone = cel_phone
+    end
+
+    def forward
+      send_to_zenvia(@msg, @cel_phone)
+    end
+  end
+end
