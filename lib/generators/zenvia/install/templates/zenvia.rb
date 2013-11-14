880789533bd79d4c1d08c08ce9f26d99e324f854
leonelmorais <leonel.morais@yahoo.com.br>
2013-11-14 00:36:43 -0200

    Add require zenvia on zenvia.rb initializer template

--- lib/generators/zenvia/install/templates/zenvia.rb
+++ lib/generators/zenvia/install/templates/zenvia.rb
@@ -1,6 +1,8 @@
+require 'zenvia'
+
 Zenvia.configure do |config|
   config.account  = 'YOUR ZENVIA ACCOUNT'
   config.code     = 'YOUR ZENVIA CODE'
-  config.dispatch = 'ZENVIA DISPATCH METHOD. recommended "sender" '
+  config.dispatch = 'ZENVIA DISPATCH METHOD'
   config.from     = 'YOUR APPLICATION NAME'
 end
