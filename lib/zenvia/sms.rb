f7cfc81a60170ecf71c24f4e4a6d471ab2531ab2
Araan Branco <araan10@hotmail.com>
2015-09-23 17:30:22 -0300

    Add header in request and ID in API

--- lib/zenvia/sms.rb
+++ lib/zenvia/sms.rb
@@ -4,13 +4,14 @@ module Zenvia
 
     attr_accessor :msg, :cel_phone
 
-    def initialize(msg, cel_phone)
+    def initialize(id_sms, msg, cel_phone)
+      @id_sms    = id_sms
       @msg       = msg
       @cel_phone = cel_phone
     end
 
     def forward
-      send_to_zenvia(@msg, @cel_phone)
+      send_to_zenvia(@id_sms, @msg, @cel_phone)
     end
   end
 end
